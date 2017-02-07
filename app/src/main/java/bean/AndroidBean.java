3dce20c9470545f89dfbdba5e38b7110e0f4cd97
KDisEDDY <LJY68835441@163.com>
2017-02-07 16:22:40 +0800

    change name

---
+++ app/src/main/java/bean/AndroidBean.java
@@ -0,0 +1,13 @@
+package bean;
+
+/**
+ * Title: AndroidBean
+ * Description:
+ * Copyright: Copyright (c) 2014-2016 gjfax.com
+ * Company: 广金所
+ * Author: 刘加彦
+ * Date: 2017/2/7
+ * Version: 1.0
+ */
+public class AndroidBean {
+}
