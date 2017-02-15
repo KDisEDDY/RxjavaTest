aec48c2da2b8df0d24513804cc251085e98efcd4
KDisEDDY <LJY68835441@163.com>
2017-02-15 16:05:12 +0800

    实现简单下来刷新和上拉加载，type切换功能

--- app/src/main/java/project/ljy/rxjavatest/MainActivity.java
+++ app/src/main/java/project/ljy/rxjavatest/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,7 @@ import bean.TypeItemBean.ResultsBean;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static android.support.v7.widget.RecyclerView.*;
 
 /**
  * 测试rxjava结合retrofit的效果，使用mvp模式构建
@@ -28,7 +33,10 @@ public class MainActivity extends AppCompatActivity implements IMain.View {
     @BindView(R.id.swl_refresh)
     SwipeRefreshLayout mSwipeRefreshLayout;
 
+    private LinearLayoutManager manager = null;
     private DaliyItemAdapter mAdapter = null;
+
+    private int mCurrentType = BaseConstant.TYPE_ANDROID;
     SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
         @Override
         public void onRefresh() {
@@ -36,13 +44,53 @@ public class MainActivity extends AppCompatActivity implements IMain.View {
         }
     };
 
+    class RVOnScrollListener extends OnScrollListener {
+        private SwipeRefreshLayout swipeRefreshLayout = null;
+        private int nowState = SCROLL_STATE_IDLE;
+        public RVOnScrollListener( SwipeRefreshLayout swipeRefreshLayout){
+            this.swipeRefreshLayout = swipeRefreshLayout;
+        }
+
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            switch (newState){
+                case SCROLL_STATE_IDLE:
+                    LayoutManager layoutManager = recyclerView.getLayoutManager();
+                    if(layoutManager instanceof LinearLayoutManager){
+                        LinearLayoutManager llmanager = (LinearLayoutManager) layoutManager;
+                        if(llmanager.findLastCompletelyVisibleItemPosition() == recyclerView.getAdapter().getItemCount() - 1){
+                            swipeRefreshLayout.setRefreshing(true);
+                            mPresenter.loadData(mCurrentType);
+                        } else {
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
+                    }
+                    nowState = SCROLL_STATE_IDLE;
+                    break;
+                case SCROLL_STATE_SETTLING:
+                    nowState = SCROLL_STATE_SETTLING;
+                    swipeRefreshLayout.setRefreshing(false);
+                    break;
+                case SCROLL_STATE_DRAGGING:
+                    nowState = SCROLL_STATE_DRAGGING;
+                    swipeRefreshLayout.setRefreshing(false);
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            }
+        }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
         mPresenter = new MainPresenter(this);
-        mPresenter.loadData(BaseConstant.TYPE_ANDROID);
+        mPresenter.loadData(mCurrentType);
     }
 
     @Override
@@ -66,7 +114,7 @@ public class MainActivity extends AppCompatActivity implements IMain.View {
             mAdapter = new DaliyItemAdapter(new ArrayList<ResultsBean>());
             mRecycleList.setAdapter(mAdapter);
         }
-        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager = new LinearLayoutManager(this);
         manager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecycleList.setLayoutManager(manager);
         mSwipeRefreshLayout.setColorSchemeResources(
@@ -78,6 +126,7 @@ public class MainActivity extends AppCompatActivity implements IMain.View {
 
         mSwipeRefreshLayout.setOnRefreshListener(mOnRefreshListener);
         mSwipeRefreshLayout.setRefreshing(false);
+        mRecycleList.addOnScrollListener(new RVOnScrollListener(mSwipeRefreshLayout));
     }
 
     @Override
@@ -89,10 +138,35 @@ public class MainActivity extends AppCompatActivity implements IMain.View {
             mAdapter.addList(list);
         }
         mAdapter.notifyDataSetChanged();
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
     public void requestTypeData(List<ResultsBean> list) {
 
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.fab_android:
+                mCurrentType = BaseConstant.TYPE_ANDROID;
+                break;
+            case R.id.fab_ios:
+                mCurrentType = BaseConstant.TYPE_IOS;
+                break;
+            case R.id.fab_webfront:
+                mCurrentType = BaseConstant.TYPE_WEBFRONT;
+                break;
+            case R.id.fab_fuli:
+                mCurrentType = BaseConstant.TYPE_FULI;
+                break;
+            case R.id.fab_extendresource:
+                mCurrentType = BaseConstant.TYPE_EXTENDEDRESOURCE;
+                break;
+            default:
+                break;
+        }
+        mPresenter.loadData(mCurrentType);
     }
 }
